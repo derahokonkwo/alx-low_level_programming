44f1534f6f39b857e4ff119bd065d63c5846ba1e
derahokonkwo <111268821+derahokonkwo@users.noreply.github.com>
2022-09-09 00:54:32 +0100

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point prints size of various types
+ * Description:'Write a C program that prints text exactly
+ * Return:Always 0 (Success if exited properly)
+ */
+
+int main(void)
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+
+}
