0dbe2149011753539def481c8d18fe92fb9325d3
derahokonkwo <talk2derah@gmail.com>
2022-09-09 08:44:10 +0000

    hello, world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,14 @@
 
 /**
  * main - Entry point
- * Description: does not use puts or printf
- * Return: Always 1 (success)
+ *
+ * Description: 'and that piece of art is useful
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n",59);
-return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
