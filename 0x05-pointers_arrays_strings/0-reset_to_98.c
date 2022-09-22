56a61c31f113fd5f10529371b209adcc22f387c9
derahokonkwo <talk2derah@gmail.com>
2022-09-22 15:23:09 +0000

    first pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,1 +1,11 @@
-Pointers and arrays
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter
+ * and update the value it point to 98
+ * @n: parameter take the value.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
